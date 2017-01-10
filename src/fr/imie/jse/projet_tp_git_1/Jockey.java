4dad29dc594dda6021f3687d09c8b5d4dfe681d8
JeromePavic <jeromepavic@gmail.com>
2017-01-10 12:18:18 +0100

    Poney and Jockey classes added to /s

---
+++ src/fr/imie/jse/projet_tp_git_1/Jockey.java
@@ -0,0 +1,27 @@
+package fr.imie.jse.projet_tp_git_1;
+
+public class Jockey {
+	private double poids;
+	private double taille;
+	
+	public Jockey() {
+		super();
+	}
+
+	public double getPoids() {
+		return poids;
+	}
+
+	public void setPoids(double poids) {
+		this.poids = poids;
+	}
+
+	public double getTaille() {
+		return taille;
+	}
+
+	public void setTaille(double taille) {
+		this.taille = taille;
+	}
+
+}
---
+++ src/fr/imie/jse/projet_tp_git_1/Poney.java
@@ -0,0 +1,38 @@
+package fr.imie.jse.projet_tp_git_1;
+
+public class Poney {
+
+	private double poids;
+	private double taille;
+	private String nom;
+	
+	public Poney() {
+		super();
+	}
+
+	public double getPoids() {
+		return poids;
+	}
+
+	public void setPoids(double poids) {
+		this.poids = poids;
+	}
+
+	public double getTaille() {
+		return taille;
+	}
+
+	public void setTaille(double taille) {
+		this.taille = taille;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+}
