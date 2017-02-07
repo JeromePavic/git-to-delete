7bf4ab541243126776eee7033f0ea2246ea6d4a4
Emeline Hourmand <shiro-x3@hotmail.fr>
2017-02-07 11:15:27 +0100

    Add Class Courir and Course. Modify file Poney and Jockey

---
+++ src/fr/Course.java
@@ -0,0 +1,28 @@
+package fr;
+import java.sql.Date;
+
+public class Course {
+
+	private int id;
+	private Date date;
+	
+	public Course() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
